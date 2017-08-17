379e4110889a95538f616531ab3bb4c8e9520ea4
Alexey Velikiy <gmpota@gmail.com>
2017-08-17 17:30:55 +0200

    basic sctructure & vue-admin project

---
+++ client/README.md
@@ -0,0 +1,58 @@
+# Vuestic Admin Dashboard
+
+Responsive admin dashboard template built with [Vue.js](https://vuejs.org) and [Bootstrap 4](https://v4-alpha.getbootstrap.com). Developed by [Epicmax](http://epicmax.co). Designed by [Vasili Savitski](https://github.com/xx13/)
+
+<p align="center"> 
+  <a href="http://www.epicmax.co/vuestic" target="_blank">
+    <img src="http://i.imgur.com/pMuJVVc.png" align="center" width="888px"/>
+  </a> 
+</p>
+
+## Demo
+Check it out [live](http://vuestic.epicmax.co)!
+
+## Installation
+
+``` bash
+# clone the repo 
+git clone https://github.com/epicmaxco/vuestic-admin.git
+
+# go into app's directory and install dependencies:
+cd vuestic-admin
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+## Features
+* Vue.js
+* Bootstrap 4
+* Webpack
+* Responsive layout
+* Charts (Chart.js)
+* Maps (Google, Leaflet, amMap)
+* Progress bars
+* Material forms with beautiful validation
+* 4 Form wizard types
+* Static tables and datatables
+* Login/signup pages templates
+* and many more!
+
+## How can I support developers?
+- Star our GitHub repo :star:
+- Create pull requests, submit bugs, suggest new features or documentation updates :wrench:
+- Follow us on [Twitter](https://twitter.com/epicmaxco) :feet:
+- Like our page on [Facebook](https://www.facebook.com/epicmaxco) :thumbsup:
+
+## Can I hire you guys?
+Yes!  Visit [our homepage](http://epicmax.co/) or simply leave us a message to [hello@epicmax.co](mailto:hello@epicmax.co). We will be happy to work with you!
+
+## License
+[MIT](https://github.com/epicmaxco/vuestic-admin/blob/master/LICENSE) license.
---
+++ client/src/components/dashboard/Dashboard.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="dashboard">
+    <div class="row">
+      <div class="col-md-12">
+        <vuestic-alert type="success" :withCloseBtn="true">
+          <span class="badge badge-pill badge-success">SUCCESS</span>
+          You successfully read this important alert message.
+          <i class="fa fa-close alert-close"></i>
+        </vuestic-alert>
+      </div>
+    </div>
+
+    <dashboard-info-widgets></dashboard-info-widgets>
+
+    <vuestic-widget class="no-padding no-v-padding">
+      <vuestic-tabs :names="['Overview', 'Data Visualization', 'Users & Members', 'Setup Profile']" ref="tabs">
+        <div slot="Overview">
+          <overview-tab @explore="explore"></overview-tab>
+        </div>
+        <div slot="Data Visualization">
+          <data-visualisation-tab></data-visualisation-tab>
+        </div>
+        <div slot="Users & Members">
+          <users-members-tab></users-members-tab>
+        </div>
+        <div slot="Setup Profile">
+          <setup-profile-tab></setup-profile-tab>
+        </div>
+      </vuestic-tabs>
+    </vuestic-widget>
+
+    <dashboard-bottom-widgets></dashboard-bottom-widgets>
+
+  </div>
+</template>
+
+<script>
+  import VuesticWidget from '../vuestic-components/vuestic-widget/VuesticWidget'
+  import VuesticAlert from '../vuestic-components/vuestic-alert/VuesticAlert'
+  import DashboardInfoWidgets from './DashboardInfoWidgets'
+  import VuesticTabs from '../vuestic-components/vuestic-tabs/VuesticTabs.vue'
+  import UsersMembersTab from './users-and-members-tab/UsersMembersTab.vue'
+  import SetupProfileTab from './setup-profile-tab/SetupProfileTab.vue'
+  import OverviewTab from './overview-tab/OverviewTab.vue'
+  import DataVisualisationTab from './data-visualisation-tab/DataVisualisation.vue'
+  import DashboardBottomWidgets from './DashboardBottomWidgets.vue'
+
+  export default {
+    name: 'dashboard',
+    components: {
+      DataVisualisationTab,
+      VuesticWidget,
+      VuesticAlert,
+      DashboardInfoWidgets,
+      VuesticTabs,
+      UsersMembersTab,
+      SetupProfileTab,
+      OverviewTab,
+      DashboardBottomWidgets
+    },
+    methods: {
+      explore () {
+        this.$refs.tabs.setActive('Data Visualization')
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import "../../sass/_variables.scss";
+</style>
