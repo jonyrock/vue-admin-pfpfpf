9038e4f258586b893b23c457e43fe41fc0dcd360
Alexey Velikiy <gmpota@gmail.com>
2017-08-19 22:04:03 +0300

    better script

--- server/app.js
+++ server/app.js
@@ -1,1 +1,15 @@
-console.log('server started');
+const express = require('express');
+const app = express();
+
+
+app.use('/static', express.static('public'));
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
